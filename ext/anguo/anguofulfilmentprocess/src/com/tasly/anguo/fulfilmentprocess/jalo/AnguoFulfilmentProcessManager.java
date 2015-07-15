8b921bc37486bc6f8e76cb8fc128597c76b27ee7
Kai Lv <kai.lv@sap.com>
2015-07-15 19:26:50 +0800

    add anguo extensions from ant modulegen accelerator.

---
+++ ext/anguo/anguofulfilmentprocess/src/com/tasly/anguo/fulfilmentprocess/jalo/AnguoFulfilmentProcessManager.java
@@ -0,0 +1,34 @@
+/*
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2000-2015 hybris AG
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of hybris
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with hybris.
+ *
+ *
+ */
+package com.tasly.anguo.fulfilmentprocess.jalo;
+
+import de.hybris.platform.jalo.JaloSession;
+import de.hybris.platform.jalo.extension.ExtensionManager;
+import com.tasly.anguo.fulfilmentprocess.constants.AnguoFulfilmentProcessConstants;
+
+import org.apache.log4j.Logger;
+
+@SuppressWarnings("PMD")
+public class AnguoFulfilmentProcessManager extends GeneratedAnguoFulfilmentProcessManager
+{
+	@SuppressWarnings("unused")
+	private static Logger log = Logger.getLogger( AnguoFulfilmentProcessManager.class.getName() );
+	
+	public static final AnguoFulfilmentProcessManager getInstance()
+	{
+		ExtensionManager em = JaloSession.getCurrentSession().getExtensionManager();
+		return (AnguoFulfilmentProcessManager) em.getExtension(AnguoFulfilmentProcessConstants.EXTENSIONNAME);
+	}
+	
+}
---
+++ ext/anguo/anguoinitialdata/src/com/tasly/anguo/initialdata/constants/AnguoInitialDataConstants.java
@@ -0,0 +1,27 @@
+/*
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2000-2015 hybris AG
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of hybris
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with hybris.
+ *
+ *
+ */
+package com.tasly.anguo.initialdata.constants;
+
+/**
+ * Global class for all AnguoInitialData constants.
+ */
+public final class AnguoInitialDataConstants extends GeneratedAnguoInitialDataConstants
+{
+	public static final String EXTENSIONNAME = "anguoinitialdata";
+
+	private AnguoInitialDataConstants()
+	{
+		//empty
+	}
+}
