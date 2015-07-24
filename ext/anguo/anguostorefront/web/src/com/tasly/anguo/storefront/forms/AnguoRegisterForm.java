98c8433246f59df7e0e80121962167511b1addff
chriszhang <chris.zhang01@sap.com>
2015-07-24 13:59:37 +0800

    Add agree terms checkbox.

--- ext/anguo/anguostorefront/web/src/com/tasly/anguo/storefront/forms/AnguoRegisterForm.java
+++ ext/anguo/anguostorefront/web/src/com/tasly/anguo/storefront/forms/AnguoRegisterForm.java
@@ -7,7 +7,17 @@ import de.hybris.platform.acceleratorstorefrontcommons.forms.RegisterForm;
 public class AnguoRegisterForm extends RegisterForm {
 	private String userId;
 	private UserType userType;
+	private Boolean isAgreeTerms = false;
 
+
+
+	public Boolean getIsAgreeTerms() {
+		return isAgreeTerms;
+	}
+
+	public void setIsAgreeTerms(Boolean isAgreeTerms) {
+		this.isAgreeTerms = isAgreeTerms;
+	}
 
 	public UserType getUserType() {
 		return userType;
