fe37a9c7896276a030cc4e3b4d5945b82c641eed
i319019 <mingyi.zhu01@sap.com>
2015-07-28 18:03:42 +0800

    AM-38 : update test

--- ext/anguo/anguocore/testsrc/com/tasly/anguo/core/category/impl/DefaultAnguoCategoryServiceTest.java
+++ ext/anguo/anguocore/testsrc/com/tasly/anguo/core/category/impl/DefaultAnguoCategoryServiceTest.java
@@ -14,8 +14,13 @@ import org.junit.Test;
 
 import com.tasly.anguo.core.category.AnguoCategoryService;
 
+import de.hybris.platform.catalog.CatalogVersionService;
+import de.hybris.platform.catalog.constants.CatalogConstants;
+import de.hybris.platform.catalog.model.CatalogVersionModel;
 import de.hybris.platform.category.model.CategoryModel;
+import de.hybris.platform.jalo.c2l.Language;
 import de.hybris.platform.servicelayer.ServicelayerTransactionalTest;
+import de.hybris.platform.servicelayer.session.SessionService;
 
 /**
  * @author i319019
@@ -25,23 +30,27 @@ public class DefaultAnguoCategoryServiceTest extends ServicelayerTransactionalTe
 
 	@Resource
 	private AnguoCategoryService anguoCategoryService;
+	@Resource
+	private SessionService sessionService;
+	@Resource
+	private CatalogVersionService catalogVersionService; 
 	
 	@Before
 	public void setUp() throws Exception
 	{
 		importCsv("/test/anguoCategoryServiceTest.csv", "utf-8");
+		CatalogVersionModel catalogVersionModel = catalogVersionService.getCatalogVersion("anguoProductCatalog", "Online");
+		sessionService.getCurrentSession().setAttribute(CatalogConstants.SESSION_CATALOG_VERSIONS, catalogVersionModel);
+		Language lang = jaloSession.getC2LManager().getLanguageByIsoCode("zh");
+		jaloSession.getSessionContext().setLanguage(lang);
 	}
 	
 	@Test
 	public void getCategoriesByKeywordTest()
 	{
-		String keyword = "精";
 		int totalCount = 3;
+		String keyword = "人参";
 		List<CategoryModel> results = anguoCategoryService.getCategoriesByKeyword(keyword, totalCount);
-		assertEquals(results.size(), 2);
-		
-		String keyword2 = "人参";
-		List<CategoryModel> results2 = anguoCategoryService.getCategoriesByKeyword(keyword2, totalCount);
-		assertEquals(results2.size(), 1);
+		assertEquals(results.size(), 1);
 	}
 }
