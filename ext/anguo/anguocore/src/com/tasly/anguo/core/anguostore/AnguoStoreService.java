80a7befbf28c7edaf6d8c83839398d834b1f3c50
An, Ming <ming.an@sap.com>
2015-08-04 17:56:57 +0800

    add comments

--- ext/anguo/anguocore/src/com/tasly/anguo/core/anguostore/AnguoStoreService.java
+++ ext/anguo/anguocore/src/com/tasly/anguo/core/anguostore/AnguoStoreService.java
@@ -12,17 +12,35 @@ import com.tasly.anguo.core.model.AnguoStoreTempModel;
 public interface AnguoStoreService
 {
 	/**
-	 * 根据店铺
+	 * 根据店铺Id找到店铺模版
 	 * 
 	 * @param storeId
 	 * @return contentCatalog 店铺模版
 	 */
 	public ContentCatalog getStoreContentCatalog(String storeId);
 
+	/**
+	 * 拷贝临时店铺数据到店铺对象中
+	 * 
+	 * @param temp
+	 * @param store
+	 * @param approved
+	 *           是否已经批准
+	 */
 	public void copyTempToAnguoStore(AnguoStoreTempModel temp, AnguoStoreModel store, boolean approved);
 
+	/**
+	 * 批准店铺创建
+	 * 
+	 * @param tempStore
+	 */
 	public void approveAnguoStoreCreation(AnguoStoreTempModel tempStore);
 
+	/**
+	 * 批准店铺修改
+	 * 
+	 * @param tempStore
+	 */
 	public void approveAnguoStoreModification(AnguoStoreTempModel tempStore);
 
 }
