1952f7b284ee744d974643b613679ead24376794
i313922 <gang.bao@sap.com>
2015-08-05 10:11:12 +0800

    BuyerCenter Controller

---
+++ ext/anguo/anguostorefront/web/src/com/tasly/anguo/storefront/controllers/pages/AnguoBuyerCenterController.java
@@ -0,0 +1,111 @@
+/*
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2000-2015 hybris AG
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of hybris
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with hybris.
+ *
+ *
+ */
+package com.tasly.anguo.storefront.controllers.pages;
+
+import de.hybris.platform.acceleratorstorefrontcommons.annotations.RequireHardLogIn;
+import de.hybris.platform.acceleratorstorefrontcommons.breadcrumb.Breadcrumb;
+import de.hybris.platform.acceleratorstorefrontcommons.breadcrumb.ResourceBreadcrumbBuilder;
+import de.hybris.platform.acceleratorstorefrontcommons.controllers.pages.AbstractSearchPageController;
+import de.hybris.platform.acceleratorstorefrontcommons.controllers.util.GlobalMessages;
+import de.hybris.platform.acceleratorstorefrontcommons.forms.AddressForm;
+import de.hybris.platform.acceleratorstorefrontcommons.forms.UpdateEmailForm;
+import de.hybris.platform.acceleratorstorefrontcommons.forms.UpdatePasswordForm;
+import de.hybris.platform.acceleratorstorefrontcommons.forms.UpdateProfileForm;
+import de.hybris.platform.acceleratorstorefrontcommons.forms.validation.AddressValidator;
+import de.hybris.platform.acceleratorstorefrontcommons.forms.validation.EmailValidator;
+import de.hybris.platform.acceleratorstorefrontcommons.forms.validation.PasswordValidator;
+import de.hybris.platform.acceleratorstorefrontcommons.forms.validation.ProfileValidator;
+import de.hybris.platform.acceleratorstorefrontcommons.forms.verification.AddressVerificationResultHandler;
+import de.hybris.platform.cms2.exceptions.CMSItemNotFoundException;
+import de.hybris.platform.commercefacades.address.AddressVerificationFacade;
+import de.hybris.platform.commercefacades.address.data.AddressVerificationResult;
+import de.hybris.platform.commercefacades.customer.CustomerFacade;
+import de.hybris.platform.commercefacades.i18n.I18NFacade;
+import de.hybris.platform.commercefacades.order.CheckoutFacade;
+import de.hybris.platform.commercefacades.order.OrderFacade;
+import de.hybris.platform.commercefacades.order.data.CCPaymentInfoData;
+import de.hybris.platform.commercefacades.order.data.OrderData;
+import de.hybris.platform.commercefacades.order.data.OrderHistoryData;
+import de.hybris.platform.commercefacades.user.UserFacade;
+import de.hybris.platform.commercefacades.user.data.AddressData;
+import de.hybris.platform.commercefacades.user.data.CountryData;
+import de.hybris.platform.commercefacades.user.data.CustomerData;
+import de.hybris.platform.commercefacades.user.data.RegionData;
+import de.hybris.platform.commercefacades.user.data.TitleData;
+import de.hybris.platform.commercefacades.user.exceptions.PasswordMismatchException;
+import de.hybris.platform.commerceservices.address.AddressVerificationDecision;
+import de.hybris.platform.commerceservices.customer.DuplicateUidException;
+import de.hybris.platform.commerceservices.search.pagedata.PageableData;
+import de.hybris.platform.commerceservices.search.pagedata.SearchPageData;
+import de.hybris.platform.servicelayer.exceptions.UnknownIdentifierException;
+import de.hybris.platform.util.Config;
+import com.tasly.anguo.storefront.controllers.ControllerConstants;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
+import org.springframework.context.annotation.Scope;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+
+/**
+ * Controller for home page
+ */
+@Controller
+@Scope("tenant")
+@RequestMapping("/anguo-buyercenter")
+public class AnguoBuyerCenterController extends AbstractSearchPageController
+{
+
+	// CMS Pages
+	private static final String BUYERCENTER_CMS_PAGE = "buyercenter";
+
+	private static final Logger LOG = Logger.getLogger(AnguoBuyerCenterController.class);
+
+	@Resource(name = "buyerCenterBreadcrumbBuilder")
+	private ResourceBreadcrumbBuilder buyerCenterBreadcrumbBuilder;
+
+
+	@RequestMapping(method = RequestMethod.GET)
+	@RequireHardLogIn
+	public String account(final Model model) throws CMSItemNotFoundException
+	{
+		LOG.info("进入买家中心页面。");
+		storeCmsPageInModel(model, getContentPageForLabelOrId(BUYERCENTER_CMS_PAGE));
+		setUpMetaDataForContentPage(model, getContentPageForLabelOrId(BUYERCENTER_CMS_PAGE));
+		model.addAttribute("breadcrumbs", buyerCenterBreadcrumbBuilder.getBreadcrumbs(null));
+		model.addAttribute("metaRobots", "noindex,nofollow");
+		return getViewForPage(model);
+	}
+
+}
