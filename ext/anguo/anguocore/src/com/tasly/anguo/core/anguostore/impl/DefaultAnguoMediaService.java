3cb76eb56268f2d82aff80c7f4df65dfa59fbcd2
An, Ming <ming.an@sap.com>
2015-08-06 09:22:18 +0800

    init media service

--- ext/anguo/anguocore/src/com/tasly/anguo/core/anguostore/impl/DefaultAnguoMediaService.java
+++ ext/anguo/anguocore/src/com/tasly/anguo/core/anguostore/impl/DefaultAnguoMediaService.java
@@ -1,35 +1,48 @@
 package com.tasly.anguo.core.anguostore.impl;
 
 import de.hybris.platform.core.model.media.MediaModel;
+import de.hybris.platform.servicelayer.media.MediaService;
+import de.hybris.platform.servicelayer.model.ModelService;
 
 import java.io.File;
 import java.util.List;
 
 import com.tasly.anguo.core.anguostore.AnguoMediaService;
-import com.tasly.anguo.core.constants.GeneratedAnguoCoreConstants.Enumerations.StoreMediaSubFolder;
+import com.tasly.anguo.core.anguostore.dao.AnguoStoreDao;
+import com.tasly.anguo.core.enums.StoreMediaSubFolder;
 
 
+/**
+ *
+ */
 public class DefaultAnguoMediaService implements AnguoMediaService
 {
+	private AnguoStoreDao anguoStoreDao;
 
+	private MediaService mediaService;
+
+	private ModelService modelService;
+
+	//hmm, maybe no use in future
 	@Override
 	public String getAnguoStoreMediaFolder(final String storeId)
 	{
-		// TODO Auto-generated method stub
-		return null;
+		return storeId;//currently the folder path equals to store ID.
 	}
 
+	//hmm, maybe no use in future
 	@Override
 	public String getMediaSubFolderPath(final String storeId, final StoreMediaSubFolder subFolder)
 	{
-		// TODO Auto-generated method stub
-		return null;
+		//AnguoStoreModel anguoStoreModel = anguoStoreDao.findAnguoStoreById(storeId);
+		return subFolder.getCode();
+
 	}
 
 	@Override
 	public boolean uploadMedia(final File file, final String mediaId, final String storeId, final String subfolderPath)
 	{
-		// TODO Auto-generated method stub
+
 		return false;
 	}
 
