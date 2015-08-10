912637ba4d741f91018ccfa0eebaeea010b282ac
Jay Wang <jay.wang@sap.com>
2015-08-10 10:20:26 +0800

    enable locallization

--- ext/anguo/anguoadmin/web/webroot/_ui/common/js/applicationRouter.js
+++ ext/anguo/anguoadmin/web/webroot/_ui/common/js/applicationRouter.js
@@ -1,16 +1,9 @@
-define(['jquery','backbone', 'underscore','navigationMenu','productsView','ordersView','paymentView','sampleView','widget/inputList' ],
+define(['jquery','backbone', 'underscore','navigationMenu','productsView','ordersView','paymentView','sampleView',
+    
+    'widget/inputList' ],
        function($,Backbone,_, Navigation, Products,Orders,Payment,Sample){
     "use strict";
 
-    var __config = [
-         {"label":"用户管理", "link": "#users", children:[{"label":"企业用户管理", "link": "/enterprise-user"},{"label":"个人卖家审核", "link": "/seller"}]},
-         {"label":"店铺管理", "link": "#stores",children:[{"label":"新店铺申请审核", "link": "/new"},{"label":"店铺信息修改审核", "link": "/change"},{"label":"店铺推荐", "link": "/recommend"}]},
-         {"label":"订单管理", "link": "#orders"},
-         {"label":"支付管理", "link": "#payment"},
-         {"label":"目录管理", "link": "#category"},
-         {"label":"商品管理", "link": "#products",children:[{"label":"商品审核", "link": "/check"},{"label":"商品推荐", "link": "/recommend"}]},
-         {"label":"示例", "link": "#sample"}
-    ];
 
     var i = 0;
 var AppRouter = Backbone.Router.extend({
@@ -113,9 +106,24 @@ var AppRouter = Backbone.Router.extend({
     
     var appRouter = new AppRouter();
 
+    require(['json!localization/ui-text-ZH.json'],function(json){
+        Backbone.__initI18n(json);
+
+     var __config = [
+         {"label":Backbone.getI18n('app.user.userManagement'), "link": "#users", children:[{"label":"企业用户管理", "link": "/enterprise-user"},{"label":"个人卖家审核", "link": "/seller"}]},
+         {"label":Backbone.getI18n('app.store.storeManagement'), "link": "#stores",children:[{"label":"新店铺申请审核", "link": "/new"},{"label":"店铺信息修改审核", "link": "/change"},{"label":"店铺推荐", "link": "/recommend"}]},
+         {"label":"订单管理", "link": "#orders"},
+         {"label":"支付管理", "link": "#payment"},
+         {"label":"目录管理", "link": "#category"},
+         {"label":"商品管理", "link": "#products",children:[{"label":"商品审核", "link": "/check"},{"label":"商品推荐", "link": "/recommend"}]},
+         {"label":"示例", "link": "#sample"}
+    ];
+
+
+        $(".nav-root").append( new Navigation(__config).render().$el);
+    });
     
-    
-    $(".nav-root").append( new Navigation(__config).render().$el);
+
     Backbone.history.start();
     return appRouter;
     
